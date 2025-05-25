b451fff545085cad013e7fdaa6a38528298f0c75
gappy <85281368+gappcio@users.noreply.github.com>
2025-05-25 20:11:44 +0200

    idk

--- Assets/Objects/World/Particles/Script_BloodDecal.gd
+++ Assets/Objects/World/Particles/Script_BloodDecal.gd
@@ -1,13 +1,16 @@
-extends Decal
+extends Node3D
 
 var decal_scale: float = 0.1;
 var alpha: float = 1.0;
 
+@onready var decal: MeshInstance3D = $Decal;
+
 func _process(delta: float) -> void:
 	decal_scale += 0.005;
 	
-	size = Vector3(decal_scale, 0.1, decal_scale);
-	modulate = Color(1.0, 1.0, 1.0, alpha);
+	decal.mesh.top_radius = decal_scale;
+	decal.mesh.bottom_radius = decal_scale;
+	#decal.mesh.material = Color(1.0, 1.0, 1.0, alpha);
 	
 	if decal_scale >= 2.0:
 		alpha -= 0.005;
